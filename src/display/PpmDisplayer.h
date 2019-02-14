39aeb932dcbff5b71e510a9d18df436b4069a584
Tyler Nychka <Tyler.Nychka@gmail.com>
2019-02-14 09:37:53 +0100

    Chapter 1 ppmviewer

---
+++ src/display/PpmDisplayer.h
@@ -0,0 +1,13 @@
+//
+// Created by Tyler nychka on 2019-02-02.
+//
+
+#ifndef RAYTRACING_PPMDISPLAYER_H
+#define RAYTRACING_PPMDISPLAYER_H
+
+class PpmDisplayer {
+public:
+    void toPpm(int nx, int ny);
+};
+
+#endif //RAYTRACING_PPMDISPLAYER_H
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Tyler nychka on 2019-02-02.
+//
+#include "display/PpmDisplayer.h"
+
+int main() {
+   PpmDisplayer ppmDisplayer;
+   ppmDisplayer.toPpm(200, 100);
+}
